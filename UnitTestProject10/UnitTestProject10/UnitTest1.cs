1e0c04cab77303b49dc9f4c6c8272321753a22f8
evgeniy.kulikov <evgeniy.kulikov@ainstainer.de>
2016-09-12 16:31:51 +0300

    init commit

---
+++ UnitTestProject10/UnitTestProject10/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject10
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("hi1");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("hi2");
+        }
+    }
+}
